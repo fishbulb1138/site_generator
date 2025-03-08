79cae1b30ccf4bc5635b296f1a3b9e74f8584527
fishbulb1138 <dssanche@outlook.com>
2025-03-08 11:04:41 -0500

    added test files, updated main

--- src/main.py
+++ src/main.py
@@ -1,1 +1,10 @@
+from enum import Enum
+from textnode import TextType, TextNode
+
 print("Hello World")
+
+def main():
+    new_node = TextNode("I'm hungry", TextType.NORMAL, "https://www.w3schools.com/python/python_classes.asp" )
+    print(new_node)
+
+main()
