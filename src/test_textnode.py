2fa429017c0b4f02cb02d1c1b2ec665ca147c9af
fishbulb1138 <dssanche@outlook.com>
2025-03-08 16:59:17 -0500

    added test_textnode.py

--- src/test_textnode.py
+++ src/test_textnode.py
@@ -8,6 +8,17 @@ class TestTextNode(unittest.TestCase):
         node2 = TextNode("This is a text node", TextType.BOLD)
         self.assertEqual(node, node2)
 
+    def test_type_not_eq(self):
+        node = TextNode("Hamburger Time", TextType.NORMAL)
+        node2 = TextNode("Hamburger Time", TextType.BOLD)
+        self.assertNotEqual(node2, node)
+
+    def test_url(self):
+        node = TextNode("No URL", TextType.NORMAL, url=None)
+        self.assertIs(node.url == None, True)
+
+
+
 
 
 if __name__ == "__main__":
